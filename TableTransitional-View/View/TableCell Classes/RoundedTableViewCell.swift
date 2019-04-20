23c58e52c66226c9f082fb2dbead0d7568115ba0
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2019-04-20 01:40:16 -0400

    Stripped default classes to comform tableview

---
+++ TableTransitional-View/View/TableCell Classes/RoundedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RoundedTableViewCell.swift
+//  TableTransitional-View
+//
+//  Created by Neftali Samarey on 4/20/19.
+//  Copyright © 2019 Neftali Samarey. All rights reserved.
+//
+
+import UIKit
+
+class RoundedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
